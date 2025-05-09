7f63ec7eca7a3a313c76318e63db93092b0170d8
erstool37 <jongwonsohn@gmail.com>
2025-05-09 17:48:15 +0900

    upload

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,5 @@
-# python src/utils/preprocess.py
-# python src/main.py --config configs/config.yaml
+python src/utils/preprocess.py
+python src/main.py --config configs/config.yaml
 
 
-python src/optimize.py --config configs/config.yaml
+# python src/optimize.py --config configs/config.yaml
