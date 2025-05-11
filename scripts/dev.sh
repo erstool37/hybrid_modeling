c26d0225226bff5ee05f79549b044749b5d86051
erstool37 <jongwonsohn@gmail.com>
2025-05-11 17:20:09 +0900

    minor fix

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,7 @@
-# python src/utils/preprocess.py
-# python src/test.py --config configs/config.yaml
+python src/utils/preprocess.py
+python src/test.py --config configs/config.yaml
 
 # python src/utils/preprocess.py
 # python src/main.py --config configs/config.yaml
 
-python src/optimize.py --config configs/config.yaml
+# python src/optimize.py --config configs/config.yaml
